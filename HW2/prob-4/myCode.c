a9660f25560fcd73b19c658ae3861f1022d89bca
Isabella Flynn <isabella.flynn@cooper.edu>
2021-10-16 06:47:48 +0000

    hw

--- HW2/prob-4/myCode.c
+++ HW2/prob-4/myCode.c
@@ -1,5 +1,16 @@
 #include <stdio.h>
 
-int main(void) {
-	printf("Hello, World!\n");
+int main() {
+	int penny, nickel, dime, quarter, coin;
+	printf("Enter a value of US currency in cents: \n");
+	scanf("%d", &coin);
+
+	quarter = coin/25;
+	dime = (coin - quarter*25)/10;
+	nickel = (coin - quarter*25-dime*10)/5;
+	penny = coin - quarter*25 - dime*10 -nickel*5;
+
+	printf("Quarters: %d, Dimes: %d, Nickels: %d, Pennies: %d\n", quarter,dime,nickel,penny);
+	return(0);
+
 }
