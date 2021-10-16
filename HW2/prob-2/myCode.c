f66c29434af0d25d310c86b229d3840dea276a34
Isabella Flynn <isabella.flynn@cooper.edu>
2021-10-16 04:27:37 +0000

    A program that converts minutes to hours

--- HW2/prob-2/myCode.c
+++ HW2/prob-2/myCode.c
@@ -1,5 +1,18 @@
 #include <stdio.h>
 
-int main(void) {
-	printf("Hello, World!\n");
+int main() {
+	int minute, hour, mins;
+	printf("Type an integer for minutes to be converted to hours: ");
+	scanf("%d", &minute);
+	if(minute >= 60){
+		hour = (minute/60);
+		mins = (minute -(60*hour));
+		printf("%d Hours and %d Minutes \n",hour,mins);
+	}
+
+	else
+		printf("0 Hours and %d Minutes \n" ,minute); 
+
+	return 0;
+
 }
